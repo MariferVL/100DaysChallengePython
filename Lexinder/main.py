5e12e601e20d564b22dc58cda7348872badc792e
Marifer VL <mfvl.1186@gmail.com>
2022-12-07 23:40:15 -0300

    Updates from pycharm

--- Lexinder/main.py
+++ Lexinder/main.py
@@ -8,77 +8,98 @@ def_displayed = []
 checked_words = []
 words_study = []
 fav_words = []
-data_dict = {}
 item_def = ""
 item_word = ""
 item_type = ""
 item_sentence = ""
 num_words = 0
 num_x = 0
+wait = ""
+
+data = pd.read_csv("Data/IELTS-Wordlist.csv")
+data_dict = data.to_dict()
 
 
-def read_data():
-    global data_dict
-    try:
-        data = pd.read_csv('Data/words_to_check.csv')
-    except FileNotFoundError:
-        data = pd.read_csv("Data/IELTS-Wordlist.csv")
-    finally:
-        data_dict = data.to_dict()
-    return random_def(data_dict)
+# try:
+#     data = pd.read_csv('Data/words_to_check.csv')
+# except FileNotFoundError:
+#     data = pd.read_csv("Data/IELTS-Wordlist.csv")
+# finally:
+
 
 # TODO Change definitions
-def random_def(data):
+def random_def():
     global item_def, item_word, item_type, item_sentence
     start_button.destroy()
     if num_words + num_x < 90:
         i = randint(1, 91)
-        item_def = data["definition"][i]
-        item_word = data["word"][i]
-        item_type = data["type"][i]
-        item_sentence = data["sentence"][i]
+        item_def = data_dict["definition"][i]
+        item_word = data_dict["word"][i]
+        item_type = data_dict["type"][i]
+        item_sentence = data_dict["sentence"][i]
         canvas.itemconfig(def_box, text=item_def)
 
-        def_displayed.append({"Word": item_word, "Type": item_type, "Definition": item_def, "Sentence": item_sentence})
+        def_displayed.append({"word": item_word, "type": item_type, "definition": item_def, "sentence": item_sentence})
 
     else:
-        canvas.create_image(0, 0, image=game_over)
+        canvas.itemconfig(canvas_bg, image=game_over)
 
-    root.after(2000, show_answer)
+    show_answer()
 
 
 def check_answer():
     global num_words
+    root.after_cancel(wait)
     checked_words.append(item_word)
     num_words = len(checked_words)
     count_check.delete('1.0', END)
     count_check.insert('end', f"{num_words}")
     clean_screen()
-    random_def(data_dict)
+    random_def()
 
 
 def wrong_answer():
     global num_x
-    words_study.append({"Word": item_word, "Type": item_type, "Definition": item_def, "Sentence": item_sentence})
+    root.after_cancel(wait)
+    words_study.append({"word": item_word, "type": item_type, "definition": item_def, "sentence": item_sentence})
     num_x = len(words_study)
     count_x.delete('1.0', END)
     count_x.insert('end', f"{num_x}")
-    data = pd.DataFrame(words_study)
-    # Append data
-    data.to_csv('Data/words_to_check.csv', mode='a')
+    unknown_data = pd.DataFrame(words_study)
+    try:
+        unknown_csv = pd.read_csv('Data/words_to_check.csv')
+    except FileNotFoundError:
+        # Create a file
+        unknown_data.to_csv('Data/words_to_check.csv', mode='w')
+    else:
+        if item_word not in unknown_csv:
+            # Append data
+            unknown_data.to_csv('Data/words_to_check.csv', mode='a', index=False, header=False)
+        else:
+            pass
     clean_screen()
-    random_def(data_dict)
+    random_def()
 
 
 # TODO Display answer
 def show_answer():
     # word_key = list(item_def.keys())[list(item_def.values()).index(item_def)]
+    wait_for_it()
     canvas.itemconfig(type_word, text=item_type)
-    root.after(2000, canvas.itemconfig(sentence_word, text=f"'{item_sentence}'"))
-    root.after(4000, canvas.itemconfig(correct_answer, text=item_word.capitalize()))
+    wait_for_it()
+    canvas.itemconfig(sentence_word, text=f"'{item_sentence}'")
+    wait_for_it()
+    canvas.itemconfig(correct_answer, text=item_word.capitalize())
 
 
-##Working
+def wait_for_it():
+    global wait
+    var = IntVar()
+    wait = root.after(3000, var.set, 1)
+    print("waiting...")
+    root.wait_variable(var)
+
+
 def clean_screen():
     canvas.itemconfig(correct_answer, text="")
     canvas.itemconfig(type_word, text="")
@@ -87,22 +108,30 @@ def clean_screen():
 
 # TODO Save in file fav words
 def save_fav():
-    fav_words.append({"Word": item_word, "Type": item_type, "Definition": item_def, "Sentence": item_sentence})
-    data = pd.DataFrame(fav_words)
-    # data.to_csv("words_to_check.csv")
-    # Append data
-    data.to_csv('Data/favorite_words.csv', mode='a')
+    fav_words.append({"word": item_word, "type": item_type, "definition": item_def, "sentence": item_sentence})
+    fav_data = pd.DataFrame(fav_words)
+    try:
+        fav_csv = pd.read_csv('Data/favorite_words.csv')
+    except FileNotFoundError:
+        # Create a file
+        fav_data.to_csv('Data/favorite_words.csv', mode='w')
+    else:
+        if item_word not in fav_csv:
+            # Append data
+            fav_data.to_csv('Data/favorite_words.csv', mode='a', index=False, header=False)
+        else:
+            pass
 
 
 # TODO Create a reverse button
 
 def reverse():
-    last_def = def_displayed[-1]
+    last_def = def_displayed[-2]
     clean_screen()
-    canvas.itemconfig(def_box, text=last_def["Definition"])
-    canvas.itemconfig(correct_answer, text=last_def["Word"])
-    canvas.itemconfig(type_word, text=last_def["Type"])
-    canvas.itemconfig(sentence_word, text=f"'{last_def['Sentence']}'")
+    canvas.itemconfig(def_box, text=last_def["definition"])
+    canvas.itemconfig(correct_answer, text=last_def["word"].capitalize())
+    canvas.itemconfig(type_word, text=last_def["type"])
+    canvas.itemconfig(sentence_word, text=f"'{last_def['sentence']}'")
 
 
 ### UI Setup ###
@@ -123,24 +152,22 @@ game_over = PhotoImage(file="Images/flash_card2.png")
 # create canvas to set flash card
 canvas = Canvas(root, width=363, height=100)
 canvas.pack(fill="both", expand=True)
-canvas.create_image(0, 0, image=bg, anchor="nw")
+canvas_bg = canvas.create_image(0, 0, image=bg, anchor="nw")
 
 # TODO Display meaning in screen
 correct_answer = canvas.create_text(74, 127, anchor="nw", width=244, fill="#FF3D74", text="", font='"Klee One" 18 bold')
-type_word = canvas.create_text(74, 160, anchor="nw", width=244, fill="#07B31C", text="", font='"Klee One" 12')
-def_box = canvas.create_text(74, 188, anchor="nw", width=244, fill="#FF3D74", text="", font='"Klee One" 14 bold')
-sentence_word = canvas.create_text(74, 277, anchor="nw", width=230, fill="#07B31C", text="",
+type_word = canvas.create_text(74, 160, anchor="nw", width=244, fill="#07B31C", text="", font='"Klee One" 12 italic')
+def_box = canvas.create_text(74, 188, anchor="nw", width=236, fill="#FF3D74", text="", font='"Klee One" 12 bold')
+sentence_word = canvas.create_text(74, 274, anchor="nw", width=238, fill="#07B31C", text="",
                                    font='"Klee One" 11 italic')
 count_x = Text(root, borderwidth=0, height=1, width=2, fg="#DBDBDB", wrap=WORD, font='Helvetica 13 bold')
 count_x.place(x=266, y=383)
 count_check = Text(root, borderwidth=0, fg="#DBDBDB", height=1, width=2, wrap=WORD, font='Helvetica 13 bold')
 count_check.place(x=326, y=382)
 
-# Number of Words guessed
-
 # TODO Create fav, correct, wrong and reverse buttons.
 # Buttons img
-start_img = PhotoImage(file="Images/start-button.png")
+start_img = PhotoImage(file="Images/start_button.png")
 x_img = PhotoImage(file="Images/x_btn.png")
 x_img = x_img.zoom(13)
 x_img = x_img.subsample(33)
@@ -155,12 +182,10 @@ reverse_img = reverse_img.zoom(6)
 reverse_img = reverse_img.subsample(33)
 
 # Buttons UI
-start_button = Button(root, image=start_img, height=113, width=248, borderwidth=0,
-                      command=read_data, relief="flat")
-canvas.create_window(56, 170, window=start_button)
-start_button.place(x=56, y=170)
+start_button = Button(root, image=start_img, borderwidth=0, height=113, width=248, command=random_def, relief="flat")
+canvas.create_window(54, 168, anchor="nw", window=start_button)
 Button(root, image=x_img, height=44, width=44, borderwidth=0, command=wrong_answer).place(x=114, y=502)
-Button(root, image=check_img, height=44, width=44, borderwidth=0, command=check_answer).place(x=203, y=503)
+Button(root, image=check_img, height=44, width=45, borderwidth=0, command=check_answer).place(x=203, y=503)
 Button(root, image=fav_img, height=29, width=29, borderwidth=0, command=save_fav).place(x=282, y=493)
 Button(root, image=reverse_img, height=29, width=29, borderwidth=0, command=reverse).place(x=52, y=493)
 
