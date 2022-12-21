fdeabd9c68eabf63dac540cb4fe7bb2e81306f0a
Marifer VL <mfvl.1186@gmail.com>
2022-12-21 09:37:37 -0300

    updating comments

--- ISSTracker/main.py
+++ ISSTracker/main.py
@@ -3,10 +3,11 @@ from datetime import *
 from time import sleep
 import smtplib
 
-MY_LAT = -33.0059115
-MY_LNG = -71.5436463
-MY_EMAIL = "conectadox.cl@gmail.com"
-MY_PASSWORD= "mnpioqealsolsdxn"
+MY_LAT = 11.1111111  # your location latitude
+MY_LNG = 11.1111111  # your location longitude
+MY_EMAIL = "username@gmail.com"
+MY_PASSWORD = "password"  # Given by Gmail To Use In Apps
+
 
 def iss_locator():
     response = requests.get(url="http://api.open-notify.org/iss-now.json")
@@ -15,23 +16,24 @@ def iss_locator():
 
     iss_lat = float(data["iss_position"]["latitude"])
     iss_lng = float(data["iss_position"]["longitude"])
-    if iss_lat in range(-28, -38) and iss_lng in range(-66, -76):
+    # Your position is within +5 or -5 degrees of the ISS position.
+    if iss_lat in range(6, 16) and iss_lng in range(6, 16):
         return True
 
-# Your position is within +5 or -5 degrees of the ISS position.
 
-
-def is_dark():
+# to confirm if is night in your location when the iss is approaching.
+def detect_night():
     parameters = {
         "lat": MY_LAT,
         "lng": MY_LNG,
-        "formatted": 0,
+        "formatted": 0,  # Change the time format to 24hrs
     }
-
+    # get info about sunrise and sunset in your location.
     response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
     response.raise_for_status()
     data = response.json()
-    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])  # hour in 24 format
+    # get only the hour from sunrise and sunset in your location.
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
     sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
     time_now = datetime.now().hour
@@ -43,6 +45,7 @@ def is_dark():
 active = True
 
 while active:
+    # if this 2 events happen you'll receive an email.
     if iss_locator() and is_dark():
         connection = smtplib.SMTP("smtp.gmail.com")
         connection.starttls()
@@ -53,6 +56,5 @@ while active:
             msg="Subject:Look Up!\n\nThe ISS is approaching you")
 
         connection.close()
-
+    # Allows you to update the process every 60 seconds if you activate the code in a daily task.
     sleep(60)
-
