e9923917fce91ae828b05d2bd52421899c385095
Marifer VL <mfvl.1186@gmail.com>
2022-12-27 17:15:34 -0300

    Day 36 90%

--- RainAlert/main.py
+++ RainAlert/main.py
@@ -1,55 +1,35 @@
-import json
 import os
 from twilio.rest import Client
-
 import requests
 
-OWM_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
-apiKey = "3f043878b843e805021e59fd3a499118"
-lat = -33.005916
-lon = -71.5414576
+# Get the Weather Data:
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
+OWM_apiKey = os.environ["Open_Weather_map_Key"]
+lat = -33.0000000
+lon = -77.0000000
 parameters = {"lat": lat,
               "lon": lon,
-              "appid": apiKey,
+              "appid": OWM_apiKey,
               "exclude": "current,minutely,daily",
               }
 
+# Set the SMS server:
 account_sid = os.environ['TWILIO_ACCOUNT_SID']
 auth_token = os.environ['TWILIO_AUTH_TOKEN']
 
-# TODO chequear clave activa
-# response = requests.get(OWM_Endpoint, params=parameters)
-# response.raise_for_status()
-# weather_data = response.json()
-# print(weather_data)
+response = requests.get(OWM_Endpoint, params=parameters)
+response.raise_for_status()
+weather_data = response.json()
 
-# Opening JSON file
-file = open("testing.json")
-# returns JSON object as a dictionary
-data = json.load(file)
-
-# Closing file
-file.close()
-
-# weatherHourly = data["hourly"]
-#
-# print(weatherHourly)
-#
-# for i in range(0, 12):
-#     if weatherHourly[i]["weather"][0]["id"] < 700:
-#         print("Bring an umbrella☔")
-
-
-# Course Version:
-weather_slice = data["hourly"][:12]
-
+# Set Rain Alert:
+weather_slice = weather_data["list"][:5]
 will_rain = False
 
-for hour_data in weather_slice:
-    condition_code = hour_data["weather"][0]["id"]
+for every3hrs_data in weather_slice:
+    condition_code = every3hrs_data["weather"][0]["id"]
     if condition_code < 700:
         will_rain = True
-
+#
 if will_rain:
     client = Client(account_sid, auth_token)
 
@@ -57,7 +37,7 @@ if will_rain:
         .create(
             body="Bring an umbrella☔",
             from_="+12517662823",
-            to="+56978665819"
+            to="+56978665819",
         )
 
     print(message.status)
--- StockApp/main.py
+++ StockApp/main.py
@@ -1,28 +1,30 @@
-import json
+import datetime
 import os
-
 import requests
 from twilio.rest import Client
+from datetime import date, timedelta, datetime
+from itertools import islice
 
 STOCK = "MSFT"
 COMPANY_NAME = "Microsoft Corp"
-priceToday = 105
-priceYesterday = 100
-difference = priceToday - priceYesterday
-percent = (difference * priceYesterday) / 100
-print(percent)
-## Use https://www.alphavantage.co
-# TODO When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+news_list = []
 
-if -5.0 >= percent or percent >= 5.0:
-    print("Get News")
-    print(difference)
+# Twilio Data:
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
 
-#TODO chequear fecha actual
-#Stock Api:
+# Current Date and time:
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S")
+current_date = date.today()
+# Yesterday date
+yesterday = current_date - timedelta(days=1)
+before_yesterday = current_date - timedelta(days=2)
+
+# Stock Api:
 alpha_apiKey = os.environ['ALPHA-VANTAGE-Key']
 Alpha_Endpoint = "https://www.alphavantage.co/query"
-parameters = {"function": "TIME_SERIES_INTRADAY",
+parameters = {"function": "TIME_SERIES_DAILY_ADJUSTED",
               "symbol": STOCK,
               "interval": "5min",
               "apikey": alpha_apiKey,
@@ -31,16 +33,38 @@ parameters = {"function": "TIME_SERIES_INTRADAY",
 response = requests.get(Alpha_Endpoint, params=parameters)
 response.raise_for_status()
 alpha_data = response.json()
-print(alpha_data)
+timeseries = alpha_data["Time Series (Daily)"]
 
 
-##  2: Use https://newsapi.org
-# TODO Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
+def take(n, iterable):
+    """Return the first n items of the iterable as a list."""
+    return list(islice(iterable, n))
+
+
+n_items = take(2, timeseries.items())
+
+i = 0
+stockClosed = []
+for key in n_items:
+    value = n_items[i][1]["4. close"]
+    stockClosed.append(value)
+    i += 1
+
+priceBYesterday = float(stockClosed[1])
+priceYesterday = float(stockClosed[0])
+difference = (priceYesterday - priceBYesterday)
+percent = round(((difference * priceBYesterday) / 100), 2)
+
+# To get the news related with the stock change date.
+lastStock = n_items[0][0]
+secondLastStock = n_items[1][0][0]
+
 # News Api:
 apiKey = os.environ['NEWSapiKey']
 NEWS_Endpoint = "https://newsapi.org/v2/everything"
 parameters = {"q": COMPANY_NAME,
-              "from": "2022-12-25",
+              "from": secondLastStock,
+              "to": lastStock,
               "sortBy": "popularity",
               "apiKey": apiKey,
               "language": "en",
@@ -54,12 +78,54 @@ news_data = response.json()
 print(news_data)
 
 
+def send_msg(number, percent_change):
+    index = 0
+    for _ in news_data:
+        news_title = news_data["articles"][i]["title"]
+        # noinspection SpellCheckingInspection
+        news_descrip = news_data["articles"][i]["description"]
+        if percent_change == "decrease":
+            change = f"📉{number}%"
+            print(f"Get News cause is {change}")
+
+        elif percent_change == "increase":
+            change = f"📈{number}%"
+            print(f"Get News cause is {change}")
+        stock_msg = f"\n{STOCK}: {change} \n📰Title: {news_title}\nDescription: {news_descrip} "
+        index += 1
+        news_list.append(stock_msg)
 
 
-## TODO 3: Use https://www.twilio.com
+important_change = False
+percent = 6
+if -5 >= percent:
+    decrease = percent * -1
+    send_msg(decrease, "decrease")
+    print("Get News cause is lower")
+    important_change = True
+elif percent >= 5:
+    send_msg(percent, "increase")
+    print("Get News cause is higher")
+    important_change = True
+else:
+    print("not big change")
+
+print(news_list)
+if important_change:
+    for msg in news_list:
+        print(msg)
+    #     client = Client(account_sid, auth_token)
+    #
+    #     message = client.messages \
+    #         .create(
+    #             body=msg,
+    #             from_="+12517662823",
+    #             to="+56978665819",
+    #         )
+    #
+    # print(message.status)
+
 # Send a seperate message with the percentage change and each article's title and description to your phone number.
-
-
 # Optional: Format the SMS message like this:
 """
 TSLA: 🔺2%
@@ -70,14 +136,3 @@ or
 Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
 Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
 """
-
-# account_sid = os.environ['TWILIO_ACCOUNT_SID']
-# auth_token = os.environ['TWILIO_AUTH_TOKEN']
-# client = Client(account_sid, auth_token)
-#
-# message = client.messages \
-#                 .create(
-#                      body="Join Earth's mightiest heroes. Like Kevin Bacon.",
-#                      from_='+15017122661',
-#                      to='+15558675310'
-#                  )
