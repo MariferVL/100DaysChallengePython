62384728c5d43dbcbdbc6bc01c0393066fdc5d3b
MariferVL <mfvl.1186@gmail.com>
2023-01-06 14:54:35 -0300

    create FlightData class

---
+++ GetMyFlight/flightData.py
@@ -0,0 +1,16 @@
+from datetime import timedelta, datetime as dt
+
+# Getting current date and time
+date = (dt.now() + timedelta(days=180)).strftime("%d/%m/%Y")
+print(date)
+
+class FlightData:
+    def __init__(self, departure_airport_code, departure_city):
+        self.price = 0
+        self.departureAirportCode = departure_airport_code
+        self.departureCity = departure_city
+        self.departureDate = date
+
+
+
+
